0b5b06637f916ed7f12654fe35746d8bfc2a9394
DeveshPurohit <100789752+DeveshPurohit@users.noreply.github.com>
2024-01-27 14:26:07 +0530

    Update WorkExperience.tsx

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -15,7 +15,7 @@ export default function WorkExperience({experiences}: Props) {
   
   return (
     <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
-         <h3 className="absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl">
+         <h3 className="absolute top-20 md:top-24 uppercase text-gray-500 tracking-[20px] text-2xl">
         Experience
       </h3>
       <div className='w-full h-[600px] mt-24 flex space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
